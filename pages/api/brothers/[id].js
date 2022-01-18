3eea7e49c0def1097c099cde24762cbce87f5625
Joseph Whiteaker III <josephsims1@gmail.com>
2022-01-18 01:30:17 -0500

    formatting handlers

--- pages/api/brothers/[id].js
+++ pages/api/brothers/[id].js
@@ -8,23 +8,28 @@ const brotherDB = new Database("brothers", {
 export default async function handler(req, res) {
 	let response = null;
 
-	
+	try {
+		if (req.method == "GET") {
+			console.clear();
+			console.log("I am getting here");
+			response = await brotherDB.getOne(req.query.id);
+		}
 
-	if (req.method == "GET") {
-		response = await brotherDB.getOne(req.query.id);
+		if (req.method == "POST") {
+			response = await brotherDB.create(req.body);
+		}
+
+		if (req.method == "PUT") {
+			response = await brotherDB.update(req.body, req.query.id);
+		}
+
+		if (req.method == "DELETE") {
+			response = await brotherDB.delete(req.query.id);
+		}
+
+		res.status(200).json(response);
+	} catch (err) {
+
+		res.json(err)
 	}
-
-	if (req.method == "POST") {
-		response = await brotherDB.create(req.body);
-	}
-
-	if (req.method == "PUT") {
-		response = await brotherDB.update(req.body, req.query.id);
-	}
-
-	if (req.method == "DELETE") {
-		response = await brotherDB.delete(req.query.id);
-	}
-
-	res.status(200).json(response);
 }
