cef9b9510fcad9b66007181f3cb145c6f140f3c4
Joseph Whiteaker III <josephsims1@gmail.com>
2022-01-18 02:08:00 -0500

    change to env variables

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -7,8 +7,8 @@ import AWS from "aws-sdk";
 AWS.config.update({
 	region: "us-east-1",
 	credentials: {
-		accessKeyId: process.env.CLUSTER_ARN,
-		secretAccessKey: process.env.SECRET_ARN,
+		accessKeyId: process.env.AWS_ACCESS_KEY_APP,
+		secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_APP,
 	},
 });
 
--- utils/auroradb.js
+++ utils/auroradb.js
@@ -3,11 +3,9 @@ const knexDataApiClient = require("knex-aurora-data-api-client");
 export const db = require("knex")({
 	client: knexDataApiClient.postgres,
 	connection: {
-		secretArn:
-			"arn:aws:secretsmanager:us-east-1:576806815502:secret:rds-db-credentials/cluster-4TUDEQVHZ3GBUVA4S3PPZKE6SU/josephaw1022-OdBYOJ", // Required
-		resourceArn:
-			"arn:aws:rds:us-east-1:576806815502:cluster:rhoprodb", // Required
-		database: "backend",
-		region: "us-east-1",
+		secretArn: process.env.SECRET_ARN,
+		resourceArn: process.env.CLUSTER_ARN, // Required
+		database: process.env.DB_NAME,
+		region: process.env.REGION_APP,
 	},
 });
