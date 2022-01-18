2085547605c2372533e9bf6eb74c3abfa6de8d5d
Joseph Whiteaker III <josephsims1@gmail.com>
2022-01-18 02:28:43 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+
+	env: {
+		REGION_APP: "us-east-1",
+		CLUSTER_ARN:
+			"arn:aws:rds:us-east-1:576806815502:cluster:rhoprodb",
+		SECRET_ARN:
+			"arn:aws:secretsmanager:us-east-1:576806815502:secret:rds-db-credentials/cluster-4TUDEQVHZ3GBUVA4S3PPZKE6SU/josephaw1022-OdBYOJ",
+		AWS_ACCESS_KEY_APP: "AKIAYMTC5Q4HB2PEDF7H",
+		AWS_SECRET_ACCESS_KEY_APP:
+			"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",
+	},
+};
