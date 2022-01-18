c4d019e735c5821ba2a29ca97d000968e584cffb
Joseph Whiteaker III <josephsims1@gmail.com>
2022-01-18 01:38:18 -0500

    aws sdk support

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,6 +2,19 @@ import "../style/tailwindconfig.css";
 import "../style/global.scss";
 import { Provider } from "react-redux";
 import {store} from "../redux"
+import AWS from 'aws-sdk';
+
+
+
+
+AWS.config.update({
+    accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID, // Generated on step 1
+    secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY, // Generated on step 1
+    region: 'us-east-1', // Must be the same as your bucket
+    signatureVersion: 'v4',
+});
+
+
 
 function MyApp({ Component, pageProps }) {
 	return (
