68e8b4e41b9a8079963dbb2f977fab353098e792
Joseph Whiteaker III <josephsims1@gmail.com>
2022-01-18 02:03:48 -0500

    update aws sdk

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,15 +4,13 @@ import { Provider } from "react-redux";
 import { store } from "../redux";
 import AWS from "aws-sdk";
 
-AWS.config.update(
-	{
-		accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID, // Generated on step 1
-		secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY, // Generated on step 1
-		region: "us-east-1", // Must be the same as your bucket
-		signatureVersion: "v4",
+AWS.config.update({
+	region: "us-east-1",
+	credentials: {
+		accessKeyId: process.env.CLUSTER_ARN,
+		secretAccessKey: process.env.SECRET_ARN,
 	},
-	true
-);
+});
 
 function MyApp({ Component, pageProps }) {
 	return (
