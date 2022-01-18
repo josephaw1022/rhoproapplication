9fa60bbab02adc3440ef9861d30539dc25159645
Joseph Whiteaker III <josephsims1@gmail.com>
2022-01-18 01:49:09 -0500

    format aws object

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,27 +1,25 @@
 import "../style/tailwindconfig.css";
 import "../style/global.scss";
 import { Provider } from "react-redux";
-import {store} from "../redux"
-import AWS from 'aws-sdk';
+import { store } from "../redux";
+import AWS from "aws-sdk";
 
-
-
-
-AWS.config.update({
-    accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID, // Generated on step 1
-    secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY, // Generated on step 1
-    region: 'us-east-1', // Must be the same as your bucket
-    signatureVersion: 'v4',
-});
-
-
+AWS.config.update(
+	{
+		accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID, // Generated on step 1
+		secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY, // Generated on step 1
+		region: "us-east-1", // Must be the same as your bucket
+		signatureVersion: "v4",
+	},
+	true
+);
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<Provider store={store}>
-		<div className="h-screen w-full">
-			<Component {...pageProps} />
-		</div>
+			<div className="h-screen w-full">
+				<Component {...pageProps} />
+			</div>
 		</Provider>
 	);
 }
