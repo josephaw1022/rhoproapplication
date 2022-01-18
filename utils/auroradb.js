1c28b8498a9735c884ba435b6adc5e1eb12560b7
Joseph Whiteaker III <josephsims1@gmail.com>
2022-01-17 22:52:38 -0500

    Update auroradb.js

--- utils/auroradb.js
+++ utils/auroradb.js
@@ -3,9 +3,11 @@ const knexDataApiClient = require("knex-aurora-data-api-client");
 export const db = require("knex")({
 	client: knexDataApiClient.postgres,
 	connection: {
-		secretArn: process.env.secret_arn, // Required
-		resourceArn: process.env.CLUSTER_ARN, // Required
-		database: process.env.DB_NAME,
-		region: process.env.REGION,
+		secretArn:
+			"arn:aws:secretsmanager:us-east-1:576806815502:secret:rds-db-credentials/cluster-4TUDEQVHZ3GBUVA4S3PPZKE6SU/josephaw1022-OdBYOJ", // Required
+		resourceArn:
+			"arn:aws:rds:us-east-1:576806815502:cluster:rhoprodb", // Required
+		database: "backend",
+		region: "us-east-1",
 	},
 });
