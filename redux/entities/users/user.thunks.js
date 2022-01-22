12a4de0dc8bb2240fdf2ba380af22fa2fa830067
Joseph Whiteaker III <josephsims1@gmail.com>
2022-01-22 18:05:14 -0500

    switch back to request instead of apis

--- redux/entities/users/user.thunks.js
+++ redux/entities/users/user.thunks.js
@@ -1,10 +1,11 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
+import { request } from "../../../utils/request";
 
 export const getUsers = createAsyncThunk(
 	"users",
 	async ({ callback, ...payload }) => {
-		const response = await axios.get(`/api/brothers`);
+		const response = await request.get(`/api/brothers`);
 		callback(null, response.data);
 	}
 );
@@ -12,22 +13,22 @@ export const getUsers = createAsyncThunk(
 export const getBrother = createAsyncThunk(
 	"brother",
 	async ({ callback, ...payload }) => {
-		const response = await axios.get(`/api/brothers/${payload.id}`);
+		const response = await request.get(`/api/brothers/${payload.id}`);
 		callback(null, response.data);
 	}
 );
 
 export const updateBrother = createAsyncThunk(
-	"update brother",
+	"update/brother",
 	async ({ callback, ...payload }) => {
 		try {
-			console.log(payload);
-			const response = await axios.put(
+			console.log("payload = ", payload);
+			const {data} = await request.put(
 				`/api/brothers/${payload.id}`,
 				payload
 			);
-			console.log(response);
-			callback(null, response.data);
+			
+			callback(null, data);
 		} catch (err) {
 			callback(err, null);
 		}
