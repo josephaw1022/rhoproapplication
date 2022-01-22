92509bf8a68285867c3eead65e42b7cfad2086fb
Joseph Whiteaker III <josephsims1@gmail.com>
2022-01-22 14:33:54 -0500

    hide nav prop and format component

--- components/layout/NavAndTab.jsx
+++ components/layout/NavAndTab.jsx
@@ -1,39 +1,41 @@
 import React from "react";
 import { TabBar } from "../tab/TabBar";
-import Navbar  from "../navbar/Navbar";
+import Navbar from "../navbar/Navbar";
 import { LoadingOrError } from "./LoadingOrError";
 
 export const NavAndTab = ({
-    icon,
-    title,
-    suffix,
-    error,
-    loading,
-    
-    selectedTab,
-    tabs,
-    tab,
-    ...props
+	icon,
+	title,
+	suffix,
+	error,
+	loading,
+    hideNav, 
+	selectedTab,
+	tabs,
+	tab,
+	...props
 }) => {
-    return (
-        <div className="h-full overflow-y-none ">
-            <Navbar
-                icon={icon || null}
-                title={title || null}
-                suffix={suffix || null}
-            />
-            <div className="flex flex-col h-full overflow-y-none ">
-                <div className="h-full grow  ">
-                    <LoadingOrError error={error} loading={loading}>
-                        {props.children}
-                    </LoadingOrError>
-                </div>
-                <TabBar
-                    selectedTab={selectedTab}
-                    tabs={tabs}
-                    setTab={tab => props.setTab(tab)}
-                />
-            </div>
-        </div>
-    );
+	return (
+		<div className="h-full overflow-y-none ">
+			<Navbar
+				icon={icon || null}
+				title={title || null}
+				suffix={suffix || null}
+			/>
+			<div className="flex flex-col h-full overflow-y-none ">
+				<div className="h-full grow  ">
+					<LoadingOrError error={error} loading={loading}>
+						{props.children}
+					</LoadingOrError>
+				</div>
+				{hideNav ? null : (
+					<TabBar
+						selectedTab={selectedTab}
+						tabs={tabs}
+						setTab={tab => props.setTab(tab)}
+					/>
+				)}
+			</div>
+		</div>
+	);
 };
