861976f9d84c7f2b96adb26ea22084dc0a3c6376
Joseph Whiteaker III <josephsims1@gmail.com>
2022-01-22 14:35:19 -0500

    updateBrother function

--- redux/entities/users/user.thunks.js
+++ redux/entities/users/user.thunks.js
@@ -1,13 +1,11 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
-
-
-const API = process.env.REACT_APP_API_ENDPOINT;
+import { request } from "../../../utils/request";
 
 export const getUsers = createAsyncThunk(
 	"users",
 	async ({ callback, ...payload }) => {
-		const response = await axios.get(`/api/brothers`);
+		const response = await request.get(`/api/brothers`);
 		callback(null, response.data);
 	}
 );
@@ -15,7 +13,24 @@ export const getUsers = createAsyncThunk(
 export const getBrother = createAsyncThunk(
 	"brother",
 	async ({ callback, ...payload }) => {
-		const response = await axios.get(`/api/brothers/${payload.id}`);
+		const response = await request.get(`/api/brothers/${payload.id}`);
 		callback(null, response.data);
+	}
+);
+
+export const updateBrother = createAsyncThunk(
+	"update brother",
+	async ({ callback, ...payload }) => {
+		try {
+			console.log(payload)
+			const response = await request.put(
+				`/api/brothers/${payload.id}`,
+				payload
+			);
+			console.log(response);
+			callback(null, response.data);
+		} catch (err) {
+			callback(err, null);
+		}
 	}
 );
