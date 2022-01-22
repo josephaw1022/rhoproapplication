2bde08af7e0e0627a9b620f59bdcd4e5a09a3a47
Joseph Whiteaker III <josephsims1@gmail.com>
2022-01-22 13:16:14 -0500

    reformat id api function for calendar

--- pages/api/calendar/[id].js
+++ pages/api/calendar/[id].js
@@ -11,18 +11,11 @@ export default async function eventhandler(req, res) {
 	if (req.method == "GET")
 		response = await eventDB.getOne(req.query.id);
 
-	if (req.method == "POST") {
-		response = await eventDB.create(req.body);
-	}
-
-	if (req.method == "PUT") {
-		console.log(req.body);
+	if (req.method == "PUT")
 		response = await eventDB.update(req.body, req.query.id);
-	}
 
-	if (req.method == "DELETE") {
+	if (req.method == "DELETE")
 		response = await eventDB.delete(req.query.id);
-	}
 
 	res.status(200).json(response);
 }
