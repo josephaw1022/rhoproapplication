1767f2d73395987d3b45d6218615460efbf88636
Joseph Whiteaker III <josephsims1@gmail.com>
2022-01-24 09:41:40 -0500

    login page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,30 +1,97 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import { Card, CardActions, CardContent } from "@mui/material";
+import { useFormik } from "formik";
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import * as Yup from "yup";
+import { PrimaryButton } from "../components/button/PrimaryButton";
+import { TextInput } from "../components/form/Field";
+import { Field } from "../components/form/FieldProps";
+import {
+	requiredEmailVal,
+	requiredTextVal,
+} from "../components/form/Validations";
+import { Navbar } from "../components/navbar/Navbar";
+import { useRouter } from "next/router";
+import { useSnackbar } from "notistack";
 
+export default function Login(props) {
+	const dispatch = useDispatch();
+	const router = useRouter();
+	const [loading, setLoading] = useState(false);
+	const { enqueueSnackbar } = useSnackbar();
+	const [formValOnSubmit, setFormValOnSubmit] = useState(null);
+	const [errorMessage, setErrorMessage] = useState(null);
 
+	const formik = useFormik({
+		initialValues: {
+			email: "",
+			password: "",
+		},
+		validationSchema: Yup.object().shape({
+			email: requiredEmailVal(),
+			password: requiredTextVal(),
+		}),
+		onSubmit: values => {
+			/**
+			 * ! remove this later
+			 */
+			enqueueSnackbar("Logged in sucessfully");
+			router.push("/apps/brothers");
+			
+			/**
+			 * @keep_this_part
+			 */
+			//     if (formValOnSubmit !== values){
+			//         dispatch(
+			//             attemptLogin({
+			//                 ...values,
+			//                 callback: (err, token) => {
+			//                     if (token) {
+			//                         console.log(token)
+			//                         enqueueSnackbar('Welcome to the application')
+			//                         navigate("/brothers")
+			//                     }
+			//                     if (err) {
+			//                         setErrorMessage(err);
+			//                         setFormValOnSubmit(values);
+			//                         enqueueSnackbar(err);
+			//                     }
+			//                 },
+			//             })
+			//         );
+			// }
+		},
+	});
 
-export default function Home() {
-  /**
-   * State 
-   */
-	const [data, setData] = useState(null);
-
-  /**
-   * Obtain the data for the page 
-   */
-	useEffect(() => {
-		const getData = async () => {
-			const resp = await axios.get("api/hello")
-      setData(resp) 
-		};
-		getData();
-	}, []);
-  
+	const Form = {
+		email: Field(formik, "email", "Email", "email"),
+		password: Field(formik, "password", "Password", "password"),
+	};
 
 	return (
-		<div>
-			<h1>hello </h1>
-      {JSON.stringify(data)}
+		<div className="h-full w-full overflow-y-hidden">
+			<Navbar title={"Rho Proteron"} suffix={null} icon={null} />
+			<div className="h-full w-full flex justify-center items-center">
+				<form onSubmit={formik.handleSubmit}>
+					<Card className=" ">
+						<CardContent>
+							<span className="text-black text-2xl p-2 text-center flex justify-center items-center mb-4 select-none">
+								login
+							</span>
+							<div className="flex justify-center items-center gap-2">
+								<div className="flex flex-row gap-4 w-full flex-wrap   ">
+									<TextInput {...Form["email"]} />
+									<TextInput {...Form["password"]} />
+								</div>
+							</div>
+						</CardContent>
+						<CardActions>
+							<PrimaryButton type="submit">Submit</PrimaryButton>
+						</CardActions>
+					</Card>
+				</form>
+			</div>
 		</div>
 	);
 }
