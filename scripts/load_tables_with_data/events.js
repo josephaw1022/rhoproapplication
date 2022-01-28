cd43ab331f4fdf022c7b93c3ad46ad4993ffcde9
Joseph Whiteaker III <josephsims1@gmail.com>
2022-01-28 14:04:12 -0500

    create files for loading tables

---
+++ scripts/load_tables_with_data/events.js
@@ -0,0 +1,17 @@
+
+const faker = require('faker')
+const uuid = require("uuid")
+
+
+
+const main = async () => { 
+
+    console.clear()
+    
+    console.log(faker.random.arrayElement([1 , 2 , 3]))
+}
+
+
+
+
+main()
