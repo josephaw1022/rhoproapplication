717db41e328dec9684dae438c5e1925622b9bd10
Joseph Whiteaker III <josephsims1@gmail.com>
2022-01-28 14:05:06 -0500

    rename main to index in create_tables

--- scripts/create_tables/index.js
+++ scripts/create_tables/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const DbTable = require("./Table");
 const Fields = require("./table_constants");
-const db = require("./db_client");
+const db = require("../db_client");
 
 const createTable = async dbTableInstance => {
   await db
@@ -21,10 +21,13 @@ const deleteTable = async dbTableInstance => {
 // database tables
 const accountDB = new DbTable("accounts", Fields.account_fields);
 const jobDB = new DbTable("jobs", Fields.job_fields);
-const emergencyContactDB = new DbTable("emergency_contacts", Fields.emergency_contact_field) 
+const emergencyContactDB = new DbTable("emergency_contacts", Fields.emergency_contact_fields) 
+const meritDB = new DbTable("merits", Fields.merit_fields)
+const demeritDB = new DbTable("demerits", Fields.demerit_fields) 
+
 
 /**
- *  Account DB
+ *  Account Tab;e
  */
 
 // createTable(accountDB)
@@ -32,17 +35,27 @@ const emergencyContactDB = new DbTable("emergency_contacts", Fields.emergency_co
 
 
 /**
- * Job DB 
+ * Job Table 
  */
 
-// createTable(jobDB);
+createTable(jobDB);
 // deleteTable(jobDB)
 
 
 
 /**
- * Emergency Contact DB
+ * Emergency Contact Table
  */
 
 // createTable(emergencyContactDB)
 // deleteTable(emergencyContactDB)
+
+
+/** 
+ *  Merit / Demerit Tables
+ */
+
+createTable(meritDB)
+// deleteTable(meritDB)
+createTable(demeritDB)
+// deleteTable(demeritDB)
