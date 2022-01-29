2906707ba718dc3d9b07b2f46e18cb7682911e59
Joseph Whiteaker III <josephsims1@gmail.com>
2022-01-29 03:51:30 -0500

    implement jobs api

---
+++ pages/api/jobs/index.js
@@ -0,0 +1,16 @@
+import Database from "../../../utils/aurora_postgres_db";
+
+const db = new Database("jobs", {
+	idField: "id",
+	voidField: "deleted",
+});
+
+export default async function handler(req, res) {
+	let response = null;
+
+	if (req.method == "GET") response = await db.getAll();
+
+	if (req.method == "POST") response = await db.create(req.body);
+
+	res.status(200).json(response);
+}
