40c7fd18de800dce4b2b2cce362c6d18a9d3ffdb
Joseph Whiteaker III <josephsims1@gmail.com>
2022-01-29 03:49:11 -0500

    fix get all brothers to not show inactive brothers

--- pages/api/brothers/index.js
+++ pages/api/brothers/index.js
@@ -1,18 +1,23 @@
 import Database from "../../../utils/aurora_postgres_db";
 
 const brotherDB = new Database("brothers", {
-	idField: "id",
-	voidField: "deleted",
+  idField: "id",
+  voidField: "deleted",
 });
 
 export default async function handler(req, res) {
-	let response = null;
+  let response = null;
 
-	if (req.method == "POST") {
-		response = await brotherDB.create(req.body);
-	}
+  if (req.method == "POST") {
+    response = await brotherDB.create(req.body);
+  }
 
-	if (req.method == "GET") response = await brotherDB.getAll();
+  if (req.method == "GET")
+    response = await brotherDB
+      .executeSQL(
+        `SELECT * FROM brothers WHERE ( deleted = false AND active = true ) ; `
+      )
+      .then(item => item.records);
 
-	res.status(200).json(response);
+  res.status(200).json(response);
 }
