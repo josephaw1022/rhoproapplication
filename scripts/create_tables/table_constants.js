8112799d17625ea6c2fd035c60ae64625b76dc51
Joseph Whiteaker III <josephsims1@gmail.com>
2022-01-29 03:52:34 -0500

    change DB variable name to DBTable

--- scripts/create_tables/table_constants.js
+++ scripts/create_tables/table_constants.js
@@ -1,4 +1,4 @@
-const DB = require("./Table");
+const DBTable = require("./Table");
 
 // types of variables
 const str = "string";
@@ -8,63 +8,81 @@ const lstr = "long_string";
 
 // Fields included in every entity
 const shared_fields = [
-  DB.field("id", str),
-  DB.field("create_date", str),
-  DB.field("update_date", str),
-  DB.field("deleted", bool),
+  DBTable.field("id", str),
+  DBTable.field("create_date", str),
+  DBTable.field("update_date", str),
+  DBTable.field("deleted", bool),
+];
+
+// fields for the account table
+const brother_fields = [
+  ...shared_fields,
+  DBTable.field("name", lstr),
+  DBTable.field("scroll_number", numb),
+  DBTable.field("cell_phone", lstr),
+  DBTable.field("active", bool),
+  DBTable.field("email", lstr),
 ];
 
 // fields for the account table
 const account_fields = [
   ...shared_fields,
-  DB.field("brother_id", str),
-  DB.field("active", bool),
-  DB.field("password", lstr),
+  DBTable.field("brother_id", str),
+  DBTable.field("password", lstr),
+];
+
+// fields for the permissions table
+const permission_fields = [
+  ...shared_fields,
+  DBTable.field("permission_group", lstr),
+  DBTable.field("account_id", lstr),
 ];
 
 // fields for the job table
 const job_fields = [
   ...shared_fields,
-  DB.field("job", lstr),
-  DB.field("event_id", lstr),
-  DB.field("brother_id", lstr),
-  DB.field("creator_id", lstr)
+  DBTable.field("job", lstr),
+  DBTable.field("event_id", lstr),
+  DBTable.field("brother_id", lstr),
+  DBTable.field("creator_id", lstr),
 ];
 
 // fields for the emergency contact table
 const emergency_contact_fields = [
   ...shared_fields,
-  DB.field("emergency_contact_name", lstr),
-  DB.field("emergency_contact_number", lstr),
-  DB.field("brother_id", lstr),
-  DB.field("comments", lstr),
+  DBTable.field("emergency_contact_name", lstr),
+  DBTable.field("emergency_contact_number", lstr),
+  DBTable.field("brother_id", lstr),
+  DBTable.field("comments", lstr),
 ];
 
 // fields for the merits table
 const merit_fields = [
   ...shared_fields,
-  DB.field("date", lstr),
-  DB.field("reason", lstr),
-  DB.field("comments", lstr),
-  DB.field("brother_id", lstr),
-  DB.field("creator_id", lstr),
+  DBTable.field("date", lstr),
+  DBTable.field("reason", lstr),
+  DBTable.field("comments", lstr),
+  DBTable.field("brother_id", lstr),
+  DBTable.field("creator_id", lstr),
 ];
 
 // fields for the merits table
 const demerit_fields = [
   ...shared_fields,
-  DB.field("date", lstr),
-  DB.field("reason", lstr),
-  DB.field("comments", lstr),
-  DB.field("brother_id", lstr),
-  DB.field("creator_id", lstr),
+  DBTable.field("date", lstr),
+  DBTable.field("reason", lstr),
+  DBTable.field("comments", lstr),
+  DBTable.field("brother_id", lstr),
+  DBTable.field("creator_id", lstr),
 ];
 
 module.exports = {
+  brother_fields,
   account_fields,
   shared_fields,
   job_fields,
   emergency_contact_fields,
   merit_fields,
   demerit_fields,
+  permission_fields,
 };
