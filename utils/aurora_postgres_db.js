f4a87848f1ee657dc68c3cf8d59626dc10da75d1
Joseph Whiteaker III <josephsims1@gmail.com>
2022-01-30 22:27:03 -0500

    fix credential names in client db

--- utils/aurora_postgres_db.js
+++ utils/aurora_postgres_db.js
@@ -1,26 +1,26 @@
-// import { db } from "../utils/client-db";
 import AWS from "aws-sdk";
 import { v4 as uuid } from "uuid";
 const knexDataApiClient = require("knex-aurora-data-api-client");
 
+
 AWS.config.update({
 	region: "us-east-1",
 	credentials: {
-		accessKeyId: process.env.AWS_ACCESS_KEY_APP,
-		secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_APP,
+		accessKeyId: String(process.env.AWS_AK_APP),
+		secretAccessKey: String(process.env.AWS_SK_APP),
 	},
 });
+
 
 const db = require("knex")({
-	client: knexDataApiClient.postgres,
-	connection: {
-		secretArn: process.env.SECRET_ARN,
-		resourceArn: process.env.CLUSTER_ARN, // Required
-		database: process.env.DB_NAME,
-		region: process.env.REGION_APP,
-	},
+  client: knexDataApiClient.postgres,
+  connection: {
+    secretArn: String(process.env.SECRET_ARN),
+    resourceArn: String(process.env.CLUSTER_ARN), // Required
+    database: String(process.env.DB_NAME),
+    region: String(process.env.REGION_APP),
+  },
 });
-
 const defaultFields = {
 	idField: "id",
 	voidField: "pvVoid",
