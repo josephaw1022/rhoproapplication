e34f5e22556f892130c55ccbb765f750b6377816
Joseph Whiteaker III <josephsims1@gmail.com>
2022-01-30 22:27:32 -0500

    change names of variables

--- pages/api/jobs/[id].js
+++ pages/api/jobs/[id].js
@@ -1,6 +1,6 @@
 import Database from "../../../utils/aurora_postgres_db";
 
-const eventDB = new Database("jobs", {
+const jobDB = new Database("jobs", {
   idField: "id",
   voidField: "deleted",
 });
@@ -9,13 +9,13 @@ export default async function handler(req, res) {
   let response = null;
 
   if (req.method == "GET")
-    response = await eventDB.getOne(req.query.id);
+    response = await jobDB.getOne(req.query.id);
 
   if (req.method == "PUT")
-    response = await eventDB.update(req.body, req.query.id);
+    response = await jobDB.update(req.body, req.query.id);
 
   if (req.method == "DELETE")
-    response = await eventDB.delete(req.query.id);
+    response = await jobDB.delete(req.query.id);
 
   res.status(200).json(response);
 }
