be35083ac80ec8762c1f2e1de1860ffa69c202cf
Joseph Whiteaker III <josephsims1@gmail.com>
2022-02-03 11:11:33 -0500

    fix common fields function time

--- scripts/load_tables_with_data/utils/handle_common_fields.js
+++ scripts/load_tables_with_data/utils/handle_common_fields.js
@@ -1,12 +1,20 @@
 const moment = require("moment");
 const uuid = require("uuid");
 
+
+function formatDateTimeForDB(datestr) {
+  if (!datestr) return datestr;
+  let date =  moment.utc(datestr, moment.ISO_8601);
+  return String(date.format("YYYY-MM-DD HH:mm:ss"));
+}
+
 const handleCommonFields = () => {
-  const now = String(moment().format("ll"));
+  let now = formatDateTimeForDB(moment())
+
   return {
     id: String(uuid.v4()),
-    create_date: now,
-    update_date: now,
+    create_date: String(now),
+    update_date: String(now),
     deleted: false,
   };
 };
