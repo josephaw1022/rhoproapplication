595f6f7a40e5068ed82c33d8886a3cc2b4665cfb
Joseph Whiteaker III <josephsims1@gmail.com>
2022-02-06 15:21:49 -0500

    fix small sql error

--- src/server/jobs/index.js
+++ src/server/jobs/index.js
@@ -4,7 +4,7 @@ import Database, { handleSQLRequest } from "../../utils/aurora_postgres_db"
 const db = new Database('jobs')
 
 export const getJobsByEvent = async (req) => { 
-    return await handleSQLRequest(`SELECT * FROM  brothers, events, jobs WHERE ( jobs.event_id = events.id AND brothers.id = jobs.brother_id AND events.id = '${req.query.id}') ; `)
+    return await handleSQLRequest(`SELECT * FROM  events, jobs, brothers WHERE ( jobs.event_id = events.id AND brothers.id = jobs.brother_id AND events.id = '${req.query.id}') ; `)
 }
 
 export const getJob = async req => { 
